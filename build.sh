bcca111f83cd6716718ba034ddc549bfbe739347
castelbaker <castellabakery@gmail.com>
2023-11-09 13:19:39 +0900

    add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+version="0.0.1"
+registry="localhost:32500"
+imageName="deploytest"
+tarName="deploytest.tar"
+buildPath="/build/deploytest"
+
+sudo docker build -t $registry/$imageName:$version $buildPath
+sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
+sudo ctr i import $buildPath/$tarName
+sudo ctr i push $registry/$imageName:$version
+
+sudo rm -rf $buildPath/*

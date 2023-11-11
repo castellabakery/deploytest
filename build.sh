473bf2601bb350016e7c04c09252e0deb0232783
castellabakery <castellabakery@gmail.com>
2023-11-11 17:56:29 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 version="0.1.2"
-registry="localhost:32500"
+registry="registry.pastelcloud.store"
 imageName="deploytest"
 tarName="deploytest.tar"
 buildPath="/build/deploytest"
 
 sudo docker build -t $registry/$imageName:$version $buildPath
-sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
-sudo ctr i rm $registry/$imageName:$version
-sudo ctr i import $buildPath/$tarName
-sudo ctr i push $registry/$imageName:$version
+sudo docker push $registry/$imageName:$version
+#sudo docker save -o $buildPath/$tarName $registry/$imageName:$version
+#sudo ctr i rm $registry/$imageName:$version
+#sudo ctr i import $buildPath/$tarName
+#sudo ctr i push $registry/$imageName:$version
 
-sudo rm -rf $buildPath/*
+#sudo rm -rf $buildPath/*
